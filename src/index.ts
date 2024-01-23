01b5535e4dade12d91b09bbd0b7dbfc7d43bbced
Fabian Beyerlein <dev@beyerleinf.de>
2024-01-23 23:18:46 +0100

    chore: Setup project

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+console.log('Try npm run lint/fix!');
+
+const longString =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+
+const trailing = 'Semicolon';
+
+const why = {am: 'I tabbed?'};
+
+const iWish = "I didn't have a trailing space...";
+
+export function doSomeStuff(
+  withThis: string,
+  andThat: string,
+  andThose: string[]
+) {
+  console.log(withThis);
+  console.log(andThat);
+  console.dir(andThose);
+  console.log(longString, trailing, why, iWish);
+  return;
+}
+// TODO: more examples
